4199b4a03123b8b541e80c4bc6e5455a6db1cd30
sujin <rlatnwls777@gmail.com>
2025-07-02 01:19:34 +0900

    [ feat ] Prettier 및 ESLint 설정 업데이트, TailwindCSS 플러그인 추가 및 코드 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
 
 const pretendard = localFont({
-  src: "./fonts/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
+	src: './fonts/PretendardVariable.woff2',
+	display: 'swap',
+	weight: '45 920',
+	variable: '--font-pretendard',
+})
 
 export const metadata: Metadata = {
-  title: "BabBuddy",
-  description: "메뉴 추천 서비스",
-};
+	title: 'BabBuddy',
+	description: '메뉴 추천 서비스',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="ko" className={pretendard.variable}>
-      <body className="font-pretendard min-h-screen">{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ko" className={pretendard.variable}>
+			<body className="font-pretendard min-h-screen">{children}</body>
+		</html>
+	)
 }
