ea24adf6bc3dcedbd5243c2f36a526ae6845ebc6
sujin <rlatnwls777@gmail.com>
2025-07-07 00:49:42 +0900

    [ feat ] webp의 이미지로 교체, 반응형 디자인을 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,18 +2,19 @@ import Image from 'next/image'
 
 export default function LoginPage() {
 	return (
-		<div className="flex min-h-screen w-screen overflow-hidden">
-			<div className="relative flex h-screen flex-1 flex-col items-end justify-start overflow-hidden bg-[#F76D35]">
+		<div className="flex min-h-screen w-screen flex-col overflow-hidden sm:flex-row">
+			{/* 좌측 이미지 영역: 모바일에서는 숨김, md 이상에서만 보임 */}
+			<div className="relative hidden h-60 w-full bg-[#FF5D22] sm:block sm:h-screen sm:flex-4">
 				<Image
-					src="/login.svg"
+					src="/login.webp"
 					alt="밥버디 캐릭터"
 					fill
-					className="object-contain object-left-bottom"
+					className="object-cove h-full w-full"
 					priority
 				/>
 			</div>
 			{/* 우측: 텍스트, 설명, 카카오 로그인 버튼 */}
-			<div className="flex h-screen flex-1 flex-col items-center justify-center bg-white">
+			<div className="flex h-screen w-full flex-col items-center justify-center bg-white sm:flex-5">
 				<h1 className="text-h1 mb-4 text-center font-bold">오늘 뭐 먹을지 고민 중이신가요?</h1>
 				<p className="text-body2 mb-8 text-center text-gray-500">
 					입맛 따라 기본 메뉴, 메뉴 추천 도와드릴게요 :)
