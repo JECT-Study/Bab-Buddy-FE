1099e9da9c24854d588e29084dd44ee8901ee8aa
NYeonK <skdus728@gmail.com>
2025-07-10 18:13:56 +0900

    [ feat ] 그룹방 비었을 때의 UI 구현

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -9,7 +9,9 @@ export default function GroupLayout({
 		<main className="bg-gray-5 flex min-h-screen flex-1 flex-col gap-12">
 			<Header />
 			<div className="flex justify-center">
-				<section className="flex w-3/4 rounded-[28px] bg-white">{children}</section>
+				<section className="flex w-3/4 min-w-[640px] flex-col rounded-[28px] bg-white p-6">
+					{children}
+				</section>
 			</div>
 		</main>
 	)
