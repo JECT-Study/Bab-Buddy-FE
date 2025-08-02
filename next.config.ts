47e1f90426fe407c7c47837f4ddcf9e7e0776de1
sujin <rlatnwls777@gmail.com>
2025-08-03 01:00:35 +0900

    [ feat ] 사용자 프로필 정보 API 추가 및 프로필 섹션 컴포넌트 업데이트

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const config: NextConfig = {
 	images: {
+		domains: ['img1.kakaocdn.net', 't1.kakaocdn.net'], // Add img1.kakaocdn.net and any other necessary domains
 		remotePatterns: [
 			{
 				protocol: 'https',
