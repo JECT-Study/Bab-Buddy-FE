c8af4b65a8cd0e6ef190944e76c896fbb4ea7141
sujin <rlatnwls777@gmail.com>
2025-08-11 01:31:11 +0900

    feat: 동적 라우트 최적화

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from 'next'
 
 const config: NextConfig = {
+	experimental: {
+		// 동적 라우트 최적화
+		optimizePackageImports: ['@/features'],
+	},
+	// 동적 라우트 처리 최적화
+	async rewrites() {
+		return [
+			{
+				source: '/foodSurvey/:step',
+				destination: '/foodSurvey/[step]',
+			},
+			{
+				source: '/surveyResult/:id',
+				destination: '/surveyResult/[id]',
+			},
+		]
+	},
 	images: {
 		domains: ['img1.kakaocdn.net', 't1.kakaocdn.net'], // Add img1.kakaocdn.net and any other necessary domains
 		remotePatterns: [
