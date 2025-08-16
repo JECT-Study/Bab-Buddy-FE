db1fc8f77c5dde6ce3b5b5f4127ec8ee9794e8b7
sujin <rlatnwls777@gmail.com>
2025-08-17 00:56:39 +0900

    fix: SharedSurveyResultPage와 SharedSurveyResult 컴포넌트에 id prop 추가 및 잘못된 접근 처리 로직 수정정

--- src/app/surveyResult/[id]/page.tsx
+++ src/app/surveyResult/[id]/page.tsx
@@ -1,14 +1,31 @@
-'use client'
-
 import React from 'react'
 import SharedSurveyResult from '@/features/sharedSurveyResult/components/SharedSurveyResult'
 import Header from '@/shared/components/Header'
 
-const SharedSurveyResultPage = () => {
+interface Props {
+	params: {
+		id: string
+	}
+}
+
+const SharedSurveyResultPage = ({ params }: Props) => {
+	const { id } = params
+
+	if (!id) {
+		return (
+			<>
+				<Header />
+				<div className="flex h-[calc(100vh-200px)] items-center justify-center">
+					<p className="text-lg text-gray-700">잘못된 접근입니다. 올바른 URL을 확인해주세요.</p>
+				</div>
+			</>
+		)
+	}
+
 	return (
 		<>
 			<Header />
-			<SharedSurveyResult />
+			<SharedSurveyResult id={id} />
 		</>
 	)
 }
