fedae1f25f475561bad3a92e5a0988ad1c54f537
sujin <rlatnwls777@gmail.com>
2025-09-29 23:17:07 +0900

    [ fix ] next.config.ts에서 이미지 도메인 주석 제거 및 프로토콜 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const config: NextConfig = {
   images: {
     remotePatterns: [
-      // protocol 생략하면 http, https 둘 다 자동 허용
       {
         hostname: 'img1.kakaocdn.net',
       },
@@ -13,9 +12,7 @@ const config: NextConfig = {
       {
         hostname: 'k.kakaocdn.net',
       },
-      // OpenAI는 https만
       {
-        protocol: 'https',
         hostname: '*.blob.core.windows.net',
       },
     ],
