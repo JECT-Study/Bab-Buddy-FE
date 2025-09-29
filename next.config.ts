829f2a279fc9247e4d9a9224958c77e058224ac3
sujin <rlatnwls777@gmail.com>
2025-09-29 23:02:14 +0900

    [ fix ] next.config.ts의 이미지 도메인 및 패턴 수정

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
 import type { NextConfig } from 'next'
 
 const config: NextConfig = {
-	images: {
-		domains: ['img1.kakaocdn.net', 't1.kakaocdn.net'], // Add img1.kakaocdn.net and any other necessary domains
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      // protocol 생략하면 http, https 둘 다 자동 허용
+      {
+        hostname: 'img1.kakaocdn.net',
+      },
+      {
+        hostname: 't1.kakaocdn.net',
+      },
+      {
+        hostname: 'k.kakaocdn.net',
+      },
+      // OpenAI는 https만
+      {
+        protocol: 'https',
+        hostname: '*.blob.core.windows.net',
+      },
+    ],
+  },
 }
 
 export default config
