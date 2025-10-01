061038d7ced3728d090fa394805afe20005f0378
lecto17 <khwoo_rpsit@naver.com>
2025-10-01 20:37:52 +0900

    [ chore ] 그룹방 리스트에 필요한 api 함수 생성

---
+++ src/features/group/api/groupListApi.ts
@@ -0,0 +1,42 @@
+import { serverClient } from '@/shared/api/serverClient'
+import { getServerAccessToken } from '@/shared/utils/api'
+import { redirect } from 'next/navigation'
+import type { GroupType } from '../types/group'
+import { api } from '@/shared/api/client'
+
+export const getGroups = async () => {
+	// server에서 token 가져오기
+	const token = await getServerAccessToken()
+
+	// token이 없으면 로그인 페이지로 리다이렉트
+	if (token == null) {
+		return redirect('/login')
+	}
+
+	try {
+		const response = await serverClient.get<GroupType[]>(
+			`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/voterooms/list`,
+		)
+		return response?.data
+	} catch (e) {
+		console.error('getGroups error[serverClient]: ', e)
+	}
+}
+
+export const makeGroupRoom = async (title: string) => {
+	const token = await getServerAccessToken()
+
+	if (token == null) {
+		return redirect('/login')
+	}
+
+	try {
+		const response = await api.post(
+			`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/voterooms/createroom`,
+			{ title },
+		)
+		return response?.data
+	} catch (e) {
+		console.error('makeGroupRoom error[serverClient]: ', e)
+	}
+}
