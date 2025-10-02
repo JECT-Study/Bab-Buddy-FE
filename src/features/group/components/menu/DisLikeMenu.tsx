2e5fbf660de798d9c02e32d8692859e492fa131b
lecto17 <khwoo_rpsit@naver.com>
2025-10-02 17:39:15 +0900

    [ chore ] 싫어하는 메뉴 등록 컴포넌트 생성

---
+++ src/features/group/components/menu/DisLikeMenu.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { MenuItemType, VoteMenu, type GroupDetailType } from '../../types/group'
+import MenuInputForm from '../input/MenuInputForm'
+import Image from 'next/image'
+import MenuItem from './MenuItem'
+
+interface DisLikeMenuProps {
+	room: GroupDetailType
+	setVoteMenus: React.Dispatch<React.SetStateAction<VoteMenu[]>>
+}
+export default function DisLikeMenu({ room, setVoteMenus }: DisLikeMenuProps) {
+	const [dislikedMenus, setDislikedMenus] = useState<MenuItemType[]>(room.dislikeMenuList || [])
+	return (
+		<>
+			<MenuInputForm
+				setMenus={setDislikedMenus}
+				setVoteMenus={setVoteMenus}
+				placeholder="먹기 힘든 메뉴를 작성해주세요."
+			/>
+
+			<ul className="flex max-h-[339px] flex-1 flex-col gap-2 overflow-y-scroll">
+				{dislikedMenus.map((menu) => (
+					<MenuItem key={menu.id} menu={menu} setMenus={setDislikedMenus} disableEdit={true} />
+				))}
+			</ul>
+		</>
+	)
+}
