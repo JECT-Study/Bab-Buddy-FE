029cf197f23ea7a5d217763ef42db249b30cb1bb
lecto17 <khwoo_rpsit@naver.com>
2025-10-03 21:06:25 +0900

    [ chore ] 싫어하는 음식 및 투표 데이터 없을 경우 UI 작업

--- src/features/group/components/vote-room/VoteRoom.tsx
+++ src/features/group/components/vote-room/VoteRoom.tsx
@@ -1,5 +1,6 @@
 import { type GroupDetailType, type VoteMenu } from '../../types/group'
 import Icon from '@/shared/components/Icon'
+import Image from 'next/image'
 import { useState } from 'react'
 
 interface VoteRoomProps {
@@ -22,35 +23,57 @@ export default function VoteRoom({ voteMenus, room }: VoteRoomProps) {
 				<div className="text-b3-bold flex flex-col gap-2">
 					<span>⚠️ 이런 메뉴는 못먹거나 오늘 먹고 싶지 않아요</span>
 					<ul className="flex gap-2">
-						{room.dislikeMenuList.map((menu) => (
-							<li
-								key={menu.id}
-								className="text-b3-medium text-whi rounded-[20px] bg-gray-50 px-4 py-1 text-white"
-							>
-								{menu.name}
-							</li>
-						))}
+						{room.dislikeMenuList.length > 0 ? (
+							room.dislikeMenuList.map((menu) => (
+								<li
+									key={menu.id}
+									className="text-b3-medium text-whi rounded-[20px] bg-gray-50 px-4 py-1 text-white"
+								>
+									{menu.name}
+								</li>
+							))
+						) : (
+							<li className="text-b3-medium text-gray-50">등록된 못 먹는 메뉴가 없어요.</li>
+						)}
 					</ul>
 				</div>
 			</div>
 			<ul className="flex max-h-[339px] w-full flex-1 flex-col overflow-y-auto">
-				{voteMenus.map((menu) => (
-					<li
-						key={menu.menuId}
-						className="border-gray-10 flex items-center justify-between border-b p-6"
-					>
-						<span className="text-b2-medium">{menu.name}</span>
-						<button
-							className={`flex items-center justify-center rounded-3xl px-4 py-2 ${activeMenuId === menu.menuId ? 'bg-gray-100' : 'bg-gray-5'}`}
-							onClick={() => handleClickMenu(menu)}
+				{voteMenus.length ? (
+					voteMenus.map((menu) => (
+						<li
+							key={menu.menuId}
+							className="border-gray-10 flex items-center justify-between border-b p-6"
 						>
-							<Icon.ThumbsUp
-								size={24}
-								className={`${activeMenuId === menu.menuId ? 'fill-white' : ''}`}
-							/>
-						</button>
-					</li>
-				))}
+							<span className="text-b2-medium">{menu.name}</span>
+							<button
+								className={`flex items-center justify-center rounded-3xl px-4 py-2 ${activeMenuId === menu.menuId ? 'bg-gray-100' : 'bg-gray-5'}`}
+								onClick={() => handleClickMenu(menu)}
+							>
+								<Icon.ThumbsUp
+									size={24}
+									className={`${activeMenuId === menu.menuId ? 'fill-white' : ''}`}
+								/>
+							</button>
+						</li>
+					))
+				) : (
+					<div className="flex h-full w-full flex-1 flex-col items-center justify-center bg-white p-6">
+						<Image
+							src="/assets/icons/group_blank_vote.svg"
+							alt="no_menu"
+							width={111}
+							height={115}
+							className="mb-8 object-contain"
+						/>
+						<p className="text-b3-medium flex flex-col items-center gap-2">
+							<span className="text-b1-medium text-gray-100">아직 투표할 메뉴가 없어요!</span>
+							<span className="text-b2-medium text-gray-30 text-center whitespace-pre-line">
+								{`팀원들이 메뉴 제안하기에서 메뉴를 등록하면,\n 곧 이곳에서 투표할 수 있어요.`}
+							</span>
+						</p>
+					</div>
+				)}
 			</ul>
 		</>
 	)
