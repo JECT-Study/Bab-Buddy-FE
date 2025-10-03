045d169314c82db09bc9acdc5b696eef690af849
lecto17 <khwoo_rpsit@naver.com>
2025-10-03 16:33:59 +0900

    [ chore ] 그룹방 리스트 화면 내 scroll 생기는 문제 수정

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -9,7 +9,7 @@ export default function GroupLayout({
 		<main className="bg-gray-5 flex min-h-screen w-full flex-1 flex-col">
 			<Header />
 			<div className="flex w-full justify-center">
-				<section className="flex w-full min-w-[640px] rounded-[28px] pt-[60px] pr-[70px] pb-[129px] pl-[95px]">
+				<section className="flex w-full min-w-[640px] rounded-[28px] pt-8 pr-[70px] pb-[129px] pl-[95px]">
 					{children}
 				</section>
 			</div>
