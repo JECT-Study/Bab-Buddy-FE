fac8c804d2438a27bbd969e6a915f8688e241b01
sujin <rlatnwls777@gmail.com>
2025-10-03 22:45:38 +0900

    [ fix ] 카카오 로그인 리다이렉션 방식 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
 		try {
 			// OAuth 로그인 링크 요청 (origin 포함 URL 생성)
 			const kakaoLoginUrl = await getOAuthLoginUrl()
-			window.location.href = kakaoLoginUrl
+			window.location.replace(kakaoLoginUrl)
 		} catch {
 			alert('카카오 로그인에 실패했습니다. 다시 시도해주세요.')
 		} finally {
