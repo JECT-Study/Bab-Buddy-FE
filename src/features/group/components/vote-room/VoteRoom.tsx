e7f376f66d9a9ca51d3f7633d7c11df5fdfaa614
lecto17 <khwoo_rpsit@naver.com>
2025-10-03 16:55:06 +0900

    [ chore ] 투표하기 메뉴 내 디자인 시안에 맞게 css 수정

--- src/features/group/components/vote-room/VoteRoom.tsx
+++ src/features/group/components/vote-room/VoteRoom.tsx
@@ -17,7 +17,8 @@ export default function VoteRoom({ voteMenus, room }: VoteRoomProps) {
 
 	return (
 		<>
-			<div className="bg-gray-5 mb-4 min-h-[58px] w-full rounded-3xl px-6 py-4">
+			{/* VRConetnt의 스텝 제목 높이 통일되어 있지 않아 -35px mt 추가 */}
+			<div className="bg-gray-5 -mt-[35px] mb-4 min-h-[58px] w-full rounded-3xl px-6 py-4">
 				<div className="text-b3-bold flex flex-col gap-2">
 					<span>⚠️ 이런 메뉴는 못먹거나 오늘 먹고 싶지 않아요</span>
 					<ul className="flex gap-2">
@@ -32,7 +33,7 @@ export default function VoteRoom({ voteMenus, room }: VoteRoomProps) {
 					</ul>
 				</div>
 			</div>
-			<ul className="flex w-full flex-1 flex-col overflow-y-auto">
+			<ul className="flex max-h-[339px] w-full flex-1 flex-col overflow-y-auto">
 				{voteMenus.map((menu) => (
 					<li
 						key={menu.menuId}
