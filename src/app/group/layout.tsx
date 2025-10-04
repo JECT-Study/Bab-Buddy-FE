28720d8ae71154d0f25e9bb270ace3d30dea132a
lecto17 <khwoo_rpsit@naver.com>
2025-10-04 13:26:34 +0900

    [ chore ] 그룹방 리스트 감싸는 태그 추가 및 bg 색상 적용

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -8,11 +8,9 @@ export default function GroupLayout({
 	return (
 		<main className="bg-gray-5 flex min-h-screen w-full flex-1 flex-col">
 			<Header />
-			<div className="flex w-full justify-center">
-				<section className="flex w-full min-w-[640px] rounded-[28px] pt-12 pr-[70px] pl-[95px]">
-					{children}
-				</section>
-			</div>
+			<section className="flex w-full min-w-[640px] justify-center rounded-[28px] pt-12 pr-[70px] pl-[95px]">
+				{children}
+			</section>
 		</main>
 	)
 }
--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -18,12 +18,14 @@ export default async function GroupPage() {
 
 	return (
 		<>
-			<div className="flex flex-1 flex-col items-center gap-6 py-6">
-				<h2 className="text-h2-bold">모두와 함께 음식 정하기</h2>
-				<article className="flex w-[640px] flex-1 flex-col">
-					<strong className="text-b1-bold">최근 생성된 그룹방</strong>
-					<GroupContainer groups={groups} />
-				</article>
+			<div className="flex w-[75%] flex-col items-center gap-6 rounded-3xl bg-white py-6">
+				<div className="flex flex-col items-center gap-6 py-6">
+					<h2 className="text-h2-bold">모두와 함께 음식 정하기</h2>
+					<article className="flex w-[640px] flex-1 flex-col">
+						<strong className="text-b1-bold">최근 생성된 그룹방</strong>
+						<GroupContainer groups={groups} />
+					</article>
+				</div>
 			</div>
 		</>
 	)
