7f4c853b15884d9d1f624b14faa32a0ec3bdb73b
sujin <rlatnwls777@gmail.com>
2025-10-06 02:51:35 +0900

    fix: ResultLoading 컴포넌트 스타일 수정

--- src/shared/components/ResultLoading.tsx
+++ src/shared/components/ResultLoading.tsx
@@ -8,13 +8,13 @@ export const ResultLoading = () => {
 					src="/assets/icons/loading.svg"
 					alt="로딩"
 					fill
-					className="object-contain"
+					className="animate-bounce object-contain"
 					priority
 				/>
 			</div>
-			<div className="text-b1 font-medium">잠깐만 기다려주세요!</div>
-			<div className="text-b2 color-gray-30 whitespace-pre-line">
-				{`알레르기 정보와 기호를 바탕\n으로 딱 맞는 추천을 준비하고 있어요.`}
+			<div className="text-b1-medium">잠깐만 기다려주세요!</div>
+			<div className="text-b2-medium text-gray-30 whitespace-pre-line">
+				{`알레르기 정보와 기호를 바탕으로 \n 딱 맞는 추천을 준비하고 있어요.`}
 			</div>
 		</div>
 	)
