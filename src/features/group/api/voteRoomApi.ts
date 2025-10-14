b2ea6509031fc9cac1dbbdcb34474abce1c352ea
lecto17 <khwoo_rpsit@naver.com>
2025-10-14 15:22:53 +0900

    [ chore ] 그룹방 관련 api 함수 생성

--- src/features/group/api/voteRoomApi.ts
+++ src/features/group/api/voteRoomApi.ts
@@ -1,7 +1,12 @@
-import { api } from '@/shared/api/client'
 import { getServerAccessToken } from '@/shared/utils/api'
 import { redirect } from 'next/navigation'
-import type { GroupDetailType } from '../types/group'
+import type { GroupDetailType, MenuItemType } from '../types/group'
+import { serverClient } from '@/shared/api/serverClient'
+import { api } from '@/shared/api/client'
+
+interface ServerGroupDetailType extends Omit<GroupDetailType, 'isHostUser'> {
+	hostUser: boolean
+}
 
 // 그룹방 상세 조회
 export const getGroupDetail = async (id: string) => {
@@ -12,8 +17,16 @@ export const getGroupDetail = async (id: string) => {
 	}
 
 	try {
-		const response = await api.get<GroupDetailType>(`/api/voterooms/${id}`)
-		return response.data
+		const [groupDetail, groupDislikeFoods] = await Promise.all([
+			serverClient.get<ServerGroupDetailType>(`/api/voterooms/${id}`),
+			serverClient.get<MenuItemType[]>(`/api/voterooms/dislike/${id}`),
+		])
+
+		return {
+			...groupDetail.data,
+			isHostUser: groupDetail.data.hostUser,
+			dislikeMenuList: groupDislikeFoods.data,
+		}
 	} catch (e) {
 		console.error('getGroupDetail error[serverClient]: ', e)
 	}
@@ -28,14 +41,14 @@ export const addMenuOnVoteRoom = async (voteRoomId: string, name: string) => {
 	}
 
 	try {
-		const response = await api.post<string>(`/api/voterooms/addmenu`, { voteRoomId, name })
+		const response = await api.post<string>(`/api/menu/create`, { voteRoomId, name })
 		if (response.data == null) {
 			throw new Error('addMenu error: ', response.data)
 		}
 
 		return response.data
 	} catch (e) {
-		console.error('addMenu error[serverClient]: ', e)
+		console.error('addMenu error: ', e)
 	}
 }
 
@@ -48,12 +61,12 @@ export const updateMenuOnVoteRoom = async (menuId: string, name: string) => {
 	}
 
 	try {
-		const response = await api.patch(`/api/room/menu/update/${menuId}`, {
+		const response = await api.patch(`/api/menu/update/${menuId}`, {
 			name,
 		})
 		return response.data
 	} catch (e) {
-		console.error('updateMenuOnVoteRoom error[serverClient]: ', e)
+		console.error('updateMenuOnVoteRoom error: ', e)
 	}
 }
 
@@ -66,10 +79,52 @@ export const deleteMenuOnVoteRoom = async (menuId: string) => {
 	}
 
 	try {
-		const response = await api.delete(`/api/room/menu/delete/${menuId}`)
+		const response = await api.delete(`/api/menu/delete/${menuId}`)
 		return response.data
 	} catch (e) {
-		console.error('deleteMenuOnVoteRoom error[serverClient]: ', e)
+		console.error('deleteMenuOnVoteRoom error: ', e)
+	}
+}
+
+// 싫어하는 메뉴 등록
+export const addDislikeMenuOnVoteRoom = async (roomId: string, name: string) => {
+	const token = await getServerAccessToken()
+
+	if (token == null) {
+		return redirect('/login')
+	}
+
+	try {
+		const response = await api.post<string>(`/api/voterooms/dislike`, { roomId, name })
+		if (response.data == null) {
+			throw new Error('addDislikeMenuOnVoteRoom error: ', response.data)
+		}
+
+		return response.data
+	} catch (e) {
+		console.error('addDislikeMenuOnVoteRoom error: ', e)
+	}
+}
+
+// 싫어하는 메뉴 삭제
+export const deleteDislikeMenuOnVoteRoom = async (roomId: string, name: string) => {
+	const token = await getServerAccessToken()
+
+	if (token == null) {
+		return redirect('/login')
+	}
+
+	try {
+		const response = await api.delete<string>(`/api/voterooms/dislike`, {
+			params: { roomId, name },
+		})
+		if (response.data == null) {
+			throw new Error('deleteDislikeMenuOnVoteRoom error: ', response.data)
+		}
+
+		return response.data
+	} catch (e) {
+		console.error('deleteDislikeMenuOnVoteRoom error: ', e)
 	}
 }
 
@@ -82,12 +137,13 @@ export const voteMenu = async (voteRoomId: string, menuId: string) => {
 	}
 
 	try {
-		const response = await api.post(
-			`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/vote/register?voteRoomId=${voteRoomId}&menuId=${menuId}`,
-		)
+		const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/vote/register`, {
+			voteRoomId,
+			menuId,
+		})
 		return response?.data
 	} catch (e) {
-		console.error('makeGroupRoom error[serverClient]: ', e)
+		console.error('voteMenu error: ', e)
 	}
 }
 
@@ -100,9 +156,9 @@ export const deleteVoteRoom = async (voteRoomId: string) => {
 	}
 
 	try {
-		const response = await api.delete(`/api/voterooms/delete/${voteRoomId}`)
+		const response = await api.patch(`/api/voterooms/delete/${voteRoomId}`)
 		return response.data
 	} catch (e) {
-		console.error('deleteVoteRoom error[serverClient]: ', e)
+		console.error('deleteVoteRoom error: ', e)
 	}
 }
