8e11e37c694f8adef5f320c63e505285042c5eb0
lecto17 <khwoo_rpsit@naver.com>
2025-10-17 20:48:06 +0900

    [ chore ] mock 데이터 삭제

--- src/app/group/[id]/result/page.tsx
+++ src/app/group/[id]/result/page.tsx
@@ -1,7 +1,5 @@
-// import { getVoteResult } from '@/features/group/api/voteApi'
+import { getVoteResult } from '@/features/group/api/voteApi'
 import VoteResult from '@/features/group/components/vote-result/VoteResult'
-import type { VotingType } from '@/features/group/types/group'
-
 interface ResultPageProps {
 	params: Promise<{
 		id: string
@@ -10,55 +8,7 @@ interface ResultPageProps {
 
 export default async function ResultPage({ params }: ResultPageProps) {
 	const { id } = await params
-	console.log('id', id)
-	// const result = await getVoteResult(id)
-	const result = {
-		voteRoomId: '9b30f61e-c4f2-4480-aa23-eeda2e100075',
-		title: '1차 테스트',
-		result: {
-			topMenus: [
-				{
-					rank: 1,
-					count: 6,
-					menus: [
-						{
-							menuName: '샌드위치',
-							voteCount: 3,
-						},
-						// {
-						// 	menuName: '햄버거',
-						// 	voteCount: 3,
-						// },
-					],
-				},
-				// {
-				// 	rank: 2,
-				// 	count: 5,
-				// 	menus: [
-				// 		{
-				// 			menuName: '피자',
-				// 			voteCount: 5,
-				// 		},
-				// 	],
-				// },
-				// {
-				// 	rank: 3,
-				// 	count: 2,
-				// 	menus: [
-				// 		{
-				// 			menuName: '떡볶이',
-				// 			voteCount: 1,
-				// 		},
-				// 		{
-				// 			menuName: '치킨',
-				// 			voteCount: 1,
-				// 		},
-				// 	],
-				// },
-			],
-		},
-		menuSelectMethod: 'ROULETTE' as VotingType,
-	}
+	const result = await getVoteResult(id)
 
 	return <VoteResult result={result} />
 }
